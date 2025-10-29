148f00e7d4d584196a548b5be590618b12359b36
Andreas <239522420+anbobm@users.noreply.github.com>
2025-10-29 07:54:54 +0100

    Beispiel Fahrenheit zugefügt

--- beispiele.py
+++ beispiele.py
@@ -163,3 +163,22 @@ i /=    1
 
 i = i % 1
 i %=    1
+
+# Umrechnung Celsius Fahrenheit
+# Vom Nutzer Temperatur in Celsius einlesen
+# Daraus die Temperatur in Fahrenheit berechnen und ausgeben
+
+print('Was möchtest du tun?')
+print('[F]ahrenheit in Celsius')
+print('[C]elsius in Fahrenheit')
+
+auswahl = input()
+
+if auswahl == 'C':
+    celsius = float(input('Welche Temperatur in Celsius? '))
+    fahrenheit = celsius * 9 / 5 + 32
+    print(f'{fahrenheit} °F')
+else:
+    fahrenheit = float(input('Welche Temperatur in Fahrenheit? '))
+    celsius = (fahrenheit - 32) * 5 / 9
+    print(f'{celsius} °C')
