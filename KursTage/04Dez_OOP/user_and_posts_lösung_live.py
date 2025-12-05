6567bf03ce3615c03d46e58ff9e32f257d59cbd3
Andreas <239522420+anbobm@users.noreply.github.com>
2025-12-05 11:02:27 +0100

    Lösung für Users and Posts zugefügt

---
+++ KursTage/04Dez_OOP/user_and_posts_lösung_live.py
@@ -0,0 +1,104 @@
+import sqlite3
+
+class User:
+    def __init__(self, user_id=None, name=None):
+        self.id = user_id
+        self.name = name
+
+    def __repr__(self):
+        return f"User(id={self.id}, name='{self.name}')"
+
+
+class Post:
+    def __init__(self, post_id=None, user_id=None, title=None):
+        self.id = post_id
+        self.user_id = user_id
+        self.title = title
+
+    def __repr__(self):
+        return f"Post(id={self.id}, user_id={self.user_id}, title='{self.title}')"
+
+
+class Database:
+    # legt Verbindung zur DB an und sorgt dann für das anlegen der Tabellen
+    def __init__(self, db_name="posts.db"):
+        self._connection = sqlite3.connect(db_name)
+        self.create_tables()
+
+    # Hilfsmethode, welche die Tabellen erzeugt (IF NOT EXISTS)
+    def create_tables(self):
+        cur = self._connection.cursor()
+
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS Users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            )
+            """)
+        
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS Posts(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER NOT NULL,
+                title TEXT NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES Users(id)
+            )
+            """)
+    
+    # Fügt einen Nutzer zur DB hinzu
+    def add_user(self, user: User):
+        cur = self._connection.cursor()
+
+        cur.execute("INSERT INTO Users(name) VALUES(?)", (user.name,))
+        self._connection.commit()
+        user.id = cur.lastrowid    
+        return user
+
+    # Holt einen Nutzer aus der DB anhand seiner id
+    def get_user(self, user_id):
+        cur = self._connection.cursor()
+
+        cur.execute("SELECT id, name FROM Users WHERE id = ?", (user_id,))
+
+        id, name = cur.fetchone()
+        return User(id, name)
+
+    # Fügt der DB einen Post hinzu
+    def add_post(self, post: Post):
+        cur = self._connection.cursor()
+
+        cur.execute("INSERT INTO Posts(user_id, title) VALUES(?, ?)", (post.user_id, post.title))
+        self._connection.commit()
+        post.id = cur.lastrowid
+        return post
+
+    # Holt alle Posts die zu einer bestimmten user_id gehören aus der DB
+    def get_posts_for_user(self, user_id):
+        cur = self._connection.cursor()
+
+        cur.execute("SELECT id, user_id, title FROM Posts WHERE user_id = ?", (user_id,))
+
+        posts = []
+        for row in cur.fetchall():
+            id, user_id, title = row
+            posts.append(Post(id, user_id, title))
+
+        return posts
+
+        # Alternative mit generator expression
+        # return [Post(id, user_id, title) for id, user_id, title in cur.fetchall()]
+
+
+db = Database()
+
+# Nutzer anlegen
+bernd = db.add_user(User(name="Bernd"))
+
+# # zwei Posts für diesen Nutzer anlegen
+db.add_post(Post(user_id=bernd.id, title="Hallo Welt"))
+db.add_post(Post(user_id=bernd.id, title="Mein zweiter Post"))
+
+# Posts anzeigen
+print("User:", bernd)
+posts = db.get_posts_for_user(bernd.id)
+print("Posts:", posts)
